f786eddac7688c91d485032762d2c20d04e02964
AmitxParmar <amitparmar901@gmail.com>
2022-12-14 20:18:09 +0530

    project finished

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import SearchIcon from "./components/logo.svg"
+import MovieCard from './components/MovieCard'
 import "./App.css"
-const App = () => {
-    const [advice, setAdvice] = useState('')
 
-    const fetchAdvice = () => { // generate a random quote when called;
-        axios.get('https://api.adviceslip.com/advice')
-            .then((response) => {
-                const { advice } = response.data.slip;
-                setAdvice(advice);
-                console.log(advice);
-            })
-            .catch((e) => {
-                console.log(e.message);
-            });
-    }
+const API_URL = 'https://www.omdbapi.com?apikey=7f2e1744';
+
+const App = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        fetchAdvice(); // Fetches a random quote every time the user reloads the page.
-    }, [])
+        searchMovies("Batman");
+    }, []);
 
-    return (<>
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    };
+
+    return (
         <div className="app">
-            <div className="card">
-                <h1 className="heading">{advice}</h1>
-                <button className="button" onClick={fetchAdvice}>
-                    <span>GIVE ME ADVICE!</span>
-                </button>
-            </div>
-        </div>
-    </>);
-}
+            <h1>MovieLand</h1>
 
-export default App
+            <div className="search">
+                <input
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Search for movies"
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default App;
